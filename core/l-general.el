c615b89e4b1b06f3ac0a51025c4286779544166d
Laura <laura.viglioni@loft.com.br>
2021-02-06 14:56:22 -0300

    feat: created throw-if macro and tested it

---
+++ core/l-general.el
@@ -0,0 +1,17 @@
+;;
+;; @author Laura Viglioni
+;; 2021
+;; GNU Public License 3.0
+;;
+
+;;
+;; general related functions
+;;
+
+;;;###autoload
+(defmacro throw-if (condition &optional error-description)
+  "if condition is true, thrown an error"
+  `(if ,condition
+       (error (or ,error-description ""))))
+
+(provide 'l-general)
---
+++ test/core/test-general.el
@@ -0,0 +1,11 @@
+(load "./test/l-test-helpers.el")
+(load-test-file "l-general")
+
+(test-suit "#throw-if"
+  (context "condition is truthy"
+    (it-should "throw error with correct message"
+      (expect (throw-if t "message") :to-throw 'error '("message"))
+      (expect (throw-if t) :to-throw 'error '(""))))
+  (context "condition is falsey"
+    (it-should "not throw error"
+      (expect (throw-if nil) :not :to-throw))))
