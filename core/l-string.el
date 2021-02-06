d18e6169f0150d239fac7c2efff5aea39a412baa
Laura <laura.viglioni@loft.com.br>
2021-02-06 15:11:03 -0300

    feat: moved l-files funcs to l-string and tested all l-string funcs

--- core/l-string.el
+++ core/l-string.el
@@ -8,24 +8,25 @@
 ;; string related functions
 ;;
 
+(require 'l-functional)
+(require 'l-general)
+
 ;;;###autoload
 (defun join-path (path filename)
   "concat path and file. Adds '/' to the end of the path if necessary"
-  (if (n-and path filename)
-      (throw join-path "path or filename is nil")
-    (concat path (if (string-match-p "/$" path) "" "/") filename)))
-
+  (throw-if (any-nil? path filename) "path or filename is nil")
+  (concat path (if (string-match-p "/$" path) "" "/") filename))
 
 ;;;###autoload
 (defun file-extension (filename extension)
   "returns filename.extension"
-  (if (n-and filename extension)
-      (throw file-extension "filename or extension is nil")
-    (concat filename "." extension)))
+  (throw-if (any-nil? filename extension) "filename or extension is nil")
+  (concat filename "." extension))
 
 ;;;###autoload
 (defun regex-matches (regexp string &optional pos matches)
   "Returns a list of matches"
+  (throw-if (any-nil? regexp string) "regexp or string is nil")
   (let* ((actual-pos (or pos 0))
          (m-pos (string-match regexp string actual-pos))
          (match (and m-pos (match-string 0 string))))
@@ -35,15 +36,72 @@
 
 
 ;;;###autoload
-(defun get-string-from-file (filePath)
-  "Return filePath's file content in a string"
+(defun get-string-from-file (filepath)
+  "Return filepath's file content in a string"
+  (throw-if (any-nil? filepath) "filepath is nil")
+  (throw-if (not (file-exists-p filepath)) "file does not exists")
   (with-temp-buffer
-    (insert-file-contents filePath)
+    (insert-file-contents filepath)
     (buffer-string)))
 
 ;;;###autoload
-(defun string-from-file (file-path)
-  "Return file-path's file content."
-  (with-temp-buffer
-    (insert-file-contents file-path)
-    (buffer-string)))
+(defun remove-suffix (file-name)
+  "remove suffix of file-name"
+  (throw-if (any-nil? file-name) "file-name is nil")
+  (replace-regexp-in-string "\\.[a-z]*$" ""  file-name))
+
+;;;###autoload
+(defun go-to-fst-empty-line ()
+  "search the first empty line in buffer and go to it"
+  (beginning-of-buffer)
+  (search-forward-regexp "^$"))
+
+;;;###autoload
+(defun insert-on-fst-empty-line (text current-pos)
+  "inserts text on the first empty line of the buffer and
+    return the cursor to its position"
+  (throw-if (any-nil? text current-pos) "text or current-pos is nil")
+  (let* ((empty-line-pos (progn (go-to-fst-empty-line) (point)))
+         (is-after? (> current-pos empty-line-pos))
+         (return-pos (if is-after?
+                         (+ current-pos (length text))
+                       current-pos)))
+    (insert text)
+    (goto-char return-pos)
+    t))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;;;###autoload
+;; (defun relative-path (file1 file2)
+;;   "TEMPORARIALLY DEPRECATED"
+;;   (let* ((path1-list (split-string (file-truename file1) "/"))
+;;          (path2-list (split-string (file-truename file2) "/"))
+;;          (zipped-path (zip path1-list path2-list))
+;;          (diff-path (seq-filter (lambda (pair) "" (not (equal (head pair) (head (tail pair)))))  zipped-path))
+;;          (unzipped (unzip diff-path))
+;;          (pre-path1  (nbutlast (seq-filter #'bool (head unzipped)) 1))
+;;          (pre-path2  (seq-filter #'bool (head (tail unzipped))))
+;;          (path1 (concat "./" (s-join "/" (seq-map (lambda (el) "" "..") pre-path1))))
+;;          (filename (head (last pre-path2)))
+;;          (path2 (concat (s-join "/" pre-path2)))
+;;          (relative-path (join-path path1 path2)))
+;;     relative-path))
+
--- test/core/test-string.el
+++ test/core/test-string.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t -*-
 (load "./test/l-test-helpers.el")
 (load-test-file "l-string")
 
@@ -31,3 +32,79 @@
   (context "filename and extension exists"
     (it-should "concatenate filename with extension"
       (expect (file-extension "file" "lau") :to-equal "file.lau"))))
+
+(test-suit "#regex-matches"
+  (context "there are several matches in string"
+    (it-should "return all of them in the correct order"
+      (let ((string "match1 asdasdasda match2 adasdas match3 asd")
+            (regexp "match[0-9]"))
+        (expect (regex-matches regexp string)
+                :to-equal '("match1" "match2" "match3")))))
+  (context "there are no matchs"
+    (it-should "return nil"
+      (expect (regex-matches "nomatch" "asdasdasdas")
+              :to-be nil)))
+  (context "one of the params is nil"
+    (it-should "throw error"
+      (expect (regex-matches "regexp" nil) :to-throw)
+      (expect (regex-matches nil "string") :to-throw)
+      (expect (regex-matches nil nil) :to-throw))))
+
+(test-suit "#get-string-from-file"
+  (context "param is nil or do not exist"
+    (it-should "throw error"
+      (expect (get-string-from-file nil) :to-throw)
+      (expect (get-string-from-file "invalid filepath") :to-throw)))
+  (context "param is a valid filepath"
+    (before-each
+      (spy-on 'file-exists-p :and-return-value t)
+      (spy-on 'insert-file-contents
+              :and-call-fake (lambda (path) (insert "string"))))
+    (it-should "return file's content"
+      (expect (get-string-from-file "filepath")
+              :to-equal "string"))))
+
+(test-suit "#remove-suffix"
+  (context "param is nil"
+    (it-should "throw error"
+      (expect (remove-suffix) :to-throw )))
+  (context "param has suffix"
+    (it-should "remove suffix"
+      (expect (remove-suffix "file.lau") :to-equal "file")))
+  (context "param has no suffix"
+    (it-should "return param"
+      (expect (remove-suffix "file") :to-equal "file"))))
+
+(test-suit "#insert-on-fst-empty-line"
+  (context "any param is nil"
+    (it-should "throw error"
+      (expect (insert-on-fst-empty-line) :to-throw )
+      (expect (insert-on-fst-empty-line "text" nil) :to-throw )
+      (expect (insert-on-fst-empty-line nil 0) :to-throw )))
+  (context "parameters are valid"
+    :var* ((buffer-text "first line\n\nthird line")
+           (text "i will be there")
+           (point-after-empty-line 15)
+           (point-before-empty-line 2)
+           (expected-text "first line\ni will be there\nthird line")
+           (expected-after-point (+ point-after-empty-line (length text)))
+           test-on-temp-buffer)
+    (before-each
+      (fset 'test-on-temp-buffer (lambda (initial-point)
+                                   (with-temp-buffer
+                                     (insert buffer-text)
+                                     (goto-char initial-point)
+                                     (insert-on-fst-empty-line text (point))
+                                     (list (point) (buffer-string))))))
+    (context "cursor is after the empty line"
+      (it-should "insert text and put the cursor in the correct place"
+        (expect (test-on-temp-buffer point-after-empty-line)
+                :to-equal (list expected-after-point expected-text))))
+    (context "cursor is before the empty line"
+      (it-should "insert text without changing the cursor point"
+        (expect (test-on-temp-buffer point-before-empty-line)
+                :to-equal (list point-before-empty-line expected-text))))))
+
+
+
+
