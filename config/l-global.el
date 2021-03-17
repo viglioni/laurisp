04f1ae30cfeff2a4c694c4639784b180ff00ca98
Laura <laura.viglioni@loft.com.br>
2021-03-16 22:23:07 -0300

    chore: deleted some unused files and merged some others into global.el

--- config/l-global.el
+++ config/l-global.el
@@ -9,10 +9,32 @@
 ;;
 
 
-
+;;
 ;; flycheck
+;;
+
 (global-flycheck-mode)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
-;; company-mode 
+;;
+;; company-mode
+;;
 (global-company-mode)
+
+;;
+;; shell
+;;
+
+(setq sh-indentation 2)
+(exec-path-from-shell-initialize)
+
+;; open dotfiles with sh-mode
+(add-to-list 'auto-mode-alist '("/\\.[a-zA-Z0-09]*rc$" . sh-mode))
+(add-to-list 'auto-mode-alist '("/\[a-zA-Z0-09]*rc$" . sh-mode))
+
+;;
+;; yasnippets 
+;;
+
+(add-to-list 'yas-snippet-dirs "~/laurisp/snippets")
+(yas-global-mode 1)
