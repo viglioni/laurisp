63f7d63bfa9a88282cc9f4f88961ab937c513b8f
Laura <laura.viglioni@loft.com.br>
2021-03-17 00:29:18 -0300

    feat: created macro throw-unless

--- core/l-general.el
+++ core/l-general.el
@@ -14,4 +14,9 @@
   `(if ,condition
        (error (or ,error-description ""))))
 
+(defmacro throw-unless (condition &optional error-description)
+  "if condition is true, thrown an error"
+  `(unless ,condition
+       (error (or ,error-description ""))))
+
 (provide 'l-general)
