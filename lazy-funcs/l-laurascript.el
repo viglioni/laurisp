33081d04487fa70f4257c601c0193d72b8766dd3
Laura <laura.viglioni@loft.com.br>
2021-03-16 22:28:50 -0300

    feat: migrated from tide to lsp and created some new funcs and configs

--- lazy-funcs/l-laurascript.el
+++ lazy-funcs/l-laurascript.el
@@ -13,22 +13,6 @@
 (require 'l-string)
 (require 'l-general)
 
-;;
-;; Tide setup
-;;
-
-;;;###autoload
-(defun tide-setup-hook ()
-  (tide-setup)
-  (eldoc-mode)
-  (tide-hl-identifier-mode +1)
-  (setq web-mode-enable-auto-quoting nil)
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-code-indent-offset 2)
-  (setq web-mode-attr-indent-offset 2)
-  (setq web-mode-attr-value-indent-offset 2)
-  (setq lsp-eslint-server-command '("node" (concat (getenv "HOME") "/var/src/vscode-eslint/server/out/eslintServer.js") "--stdio"))
-  )
 
 ;;
 ;; Import JS libs from package.json
@@ -83,48 +67,6 @@
         :sources (laurascript--helm-libs-source)
         :buffer "*helm buffer source*"))
 
-
-;;
-;; explain error at point
-;;
-(setq laurascript--error-buffer "laurascript-error-at-point")
-
-;;;###autoload
-(defun laurascript--kill-error-buffer (key)
-  (interactive "k")
-  (if (get-buffer laurascript--error-buffer)
-      (kill-buffer laurascript--error-buffer)))
-
-;;;###autoload
-(defun laurascript--config-error-buffer (msg)
-  (let* ((max-text-width 100)
-         (total-margin (- (window-text-width) max-text-width))
-         (margin-lateral  (/ total-margin 2))
-         (min-lines (+ 4 (ceiling (/ (length msg) (min max-text-width (window-text-width))))))
-         (actual-height (window-body-height)))
-    (if (> total-margin 0)
-        (progn (setq left-margin-width margin-lateral)
-               (setq right-margin-width margin-lateral)
-               (set-window-buffer (selected-window) (current-buffer))))
-    (enlarge-window (- min-lines actual-height))
-    (read-only-mode)
-    (visual-line-mode)))
-
-;;;###autoload
-(defun ls-explain-error-at-point ()
-  (interactive)
-  (let ((err (flycheck-overlay-errors-at (point))))
-    (if err
-        (let* ((msg (head (mapcar 'flycheck-error-message err)))
-               (buff-name (get-buffer-create laurascript--error-buffer))
-               (error-buff (get-buffer buff-name)))
-          (display-buffer-in-side-window error-buff '((side . bottom)))
-          (switch-to-buffer-other-window error-buff)
-          (erase-buffer)
-          (insert (concat "\n" msg))
-          (laurascript--config-error-buffer msg)
-          (call-interactively 'laurascript--kill-error-buffer)))
-    (error "No error at point"))) 
 
 
 
