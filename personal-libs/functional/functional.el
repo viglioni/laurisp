aca355d8dc9842cc6711ec95db98395df7530685
Laura <laura.viglioni@loft.com.br>
2021-03-28 19:56:16 -0300

    fix: fixed -> description

--- personal-libs/functional/functional.el
+++ personal-libs/functional/functional.el
@@ -48,7 +48,7 @@
   "Pipe an argument into composed functions from left to right.
    a -> ((a -> b) (b -> c) ... (n -> m)) -> m
    e.g.:
-   (-> (+ 1) (* 2)) -> (lambda (arg1 ... argN) (* 2 (+ 1 arg1 ... argN)))"
+   (->  5 ((+ 1) (* 2))) -> 12"
   `(funcall (compose ,@(reverse  fn-list)) ,arg))
 
 ;;;###autoload
