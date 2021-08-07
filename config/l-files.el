414f794714d511304f805dbcc11c758b23c5596f
Laura <laura.viglioni@loft.com.br>
2021-08-07 13:05:52 -0300

    feat: [wip] created lib template

--- config/l-files.el
+++ config/l-files.el
@@ -8,6 +8,8 @@
 ;; files related functions
 ;;
 
+(load-lib 'org-headers-skeletons)
+
 ;;
 ;; opening files
 ;;
@@ -54,4 +56,43 @@
          (file (touch filename dir)))
     (echo-into file (format ";;\n;; @author Laura Viglioni\n;; 2021\n;; GNU Public License 3.0\n;;\n\n;;\n;; %s related functions\n;;\n\n" name))
     (find-file file)))
+
+
+(create-skeleton lib-readme
+  "  To run tests and compiling this project uses [[https://github.com/doublep/eldev][eldev]]."
+  ""
+  "* Testing"
+  "To run tests we use [[https://github.com/jorgenschaefer/emacs-buttercup/][buttercup]] and some macros defined in ~~/test/buttercup-helpers~, check out [[https://github.com/Viglioni/laurisp/tree/main/personal-libs/buttercup-helpers][buttercup-helpers repo]]."
+  "*** To run tests"
+  "    In the root of this project:"
+  "    #+begin_src shell"
+  "      $ eldev test"
+  "    #+end_src"
+  "* Compiling"
+  "*** To compile"
+  "    In the root of this project:"
+  "    #+begin_src shell"
+  "      $ eldev compile"
+  "    #+end_src")
+
+
+;;;###autoload
+(defun new-laurisp-lib (file-name)
+  (interactive "sInsert filename: ")
+  (let* ((dir (join-path personal-lib-dir file-name))
+         (test-dir (join-path dir "test")))
+    (make-directory dir)
+    (make-directory test-dir)
+    (let ((file-path (touch (concat file-name ".el") dir))
+          (readme (touch "README.org" dir))
+          (eldev (touch "Eldev" dir))
+          (test-file (touch (concat file-name "-test.el"))))
+      (echo-into file-path "laurisp-package")
+      (echo-into readme (concat "* " file-name))
+      (echo-into eldev "(eldev-use-package-archive 'melpa)\n;(eldev-use-local-dependency \"\")")
+      (echo-into test-file )
+      (find-file file-path)
+      (end-of-line)
+      (yas-expand))))
+
 
